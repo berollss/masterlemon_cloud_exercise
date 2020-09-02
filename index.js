ec47f743048d73b5b243d89ab6b4cdc5dede5587
Berta <bertalaz@gmail.com>
2020-09-02 15:22:03 +0200

    Little change to launch ci

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 
 app.get("/", function (req, res) {
   console.log(req);
-  res.send("Hello world!!!");
+  res.send("Hello world!");
 });
 
 app.get("/:name", function (req, res) {
